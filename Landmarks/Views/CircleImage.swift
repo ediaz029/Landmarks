8c6a5471ce8fe2ed8d359fdeac35be21caeb95fa
ediaz029 <ediazj03@gmail.com>
2024-02-22 17:41:59 -0600

    Add existing project files to Git

---
+++ Landmarks/Views/CircleImage.swift
@@ -0,0 +1,22 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Ernesto Diaz on 2/22/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock").clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Ernesto Diaz on 2/22/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
